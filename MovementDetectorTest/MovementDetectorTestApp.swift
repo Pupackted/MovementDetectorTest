675097962cf2f0814f6839f051a3f8258b924d03
Adrian <adrian.y.rachman@gmail.com>
2025-09-30 09:41:53 +0800

    Initial Commit

---
+++ MovementDetectorTest/MovementDetectorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovementDetectorTestApp.swift
+//  MovementDetectorTest
+//
+//  Created by Adrian Yusufa Rachman on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MovementDetectorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovementDetectorTestTests/MovementDetectorTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovementDetectorTestTests.swift
+//  MovementDetectorTestTests
+//
+//  Created by Adrian Yusufa Rachman on 30/09/25.
+//
+
+import Testing
+@testable import MovementDetectorTest
+
+struct MovementDetectorTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
